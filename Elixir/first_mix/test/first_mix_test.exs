22eff984f559138cde976ea2dd3a9cde5ee2be76
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2022-12-14 14:50:37 -0500

    common test setup

--- Elixir/first_mix/test/first_mix_test.exs
+++ Elixir/first_mix/test/first_mix_test.exs
@@ -10,8 +10,12 @@ end
 defmodule FirstMix.BucketTest do
   use ExUnit.Case, async: true
 
-  test "Store value by key" do
+  setup do
     {:ok, bucket} = FirstMix.Bucket.start_link([])
+    %{bucket: bucket}
+  end
+
+  test "Store value by key", %{bucket: bucket} do
     assert FirstMix.Bucket.get(bucket, "milk") == nil
     FirstMix.Bucket.put(bucket, "milk", 3)
     assert FirstMix.Bucket.get(bucket, "milk") == 3
