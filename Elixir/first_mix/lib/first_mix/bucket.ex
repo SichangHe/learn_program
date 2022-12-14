3f44a06e001b3c10c9010c48a8732e4584016f40
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2022-12-14 14:59:51 -0500

    FirstMix.Bucket.pop

--- Elixir/first_mix/lib/first_mix/bucket.ex
+++ Elixir/first_mix/lib/first_mix/bucket.ex
@@ -4,18 +4,24 @@ defmodule FirstMix.Bucket do
   @doc """
   Start a new bucket.
   """
-  @spec start_link([{atom(), any()}]) :: Agent.on_start()
+  @spec start_link([{atom(), any()}]) :: {:ok, pid()}
   def start_link(options), do: fn -> %{} end |> Agent.start_link(options)
 
   @doc """
   Get a value in `bucket` by `key`.
   """
-  @spec get(Agent.on_start(), any()) :: any()
+  @spec get(pid(), any()) :: any()
   def get(bucket, key), do: bucket |> Agent.get(& &1[key])
 
   @doc """
   Put `value` for `key` into `bucket`.
   """
-  @spec put(Agent.on_start(), any(), any()) :: :ok
+  @spec put(pid(), any(), any()) :: :ok
   def put(bucket, key, value), do: bucket |> Agent.update(&(&1 |> Map.put(key, value)))
+
+  @doc """
+  Pop `key` and return its corresponding `value` from `bucket`.
+  """
+  @spec pop(pid(), any()) :: any()
+  def pop(bucket, key), do: bucket |> Agent.get_and_update(&Map.pop(&1, key))
 end
--- Elixir/first_mix/test/first_mix_test.exs
+++ Elixir/first_mix/test/first_mix_test.exs
@@ -9,15 +9,22 @@ end
 
 defmodule FirstMix.BucketTest do
   use ExUnit.Case, async: true
+  import FirstMix.Bucket
 
   setup do
-    {:ok, bucket} = FirstMix.Bucket.start_link([])
+    {:ok, bucket} = start_link([])
     %{bucket: bucket}
   end
 
   test "Store value by key", %{bucket: bucket} do
-    assert FirstMix.Bucket.get(bucket, "milk") == nil
-    FirstMix.Bucket.put(bucket, "milk", 3)
-    assert FirstMix.Bucket.get(bucket, "milk") == 3
+    assert get(bucket, "milk") == nil
+    put(bucket, "milk", 3)
+    assert get(bucket, "milk") == 3
+  end
+
+  test "Pop out key", %{bucket: bucket} do
+    put(bucket, "milk", 3)
+    assert pop(bucket, "milk") == 3
+    assert get(bucket, "milk") == nil
   end
 end
