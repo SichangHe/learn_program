b08dbd6b158348059702ee7837db86557871217f
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2022-12-25 11:41:56 -0500

    bare echo server using gen_tcp

--- Elixir/first_umbrella/apps/first_mix_server/lib/first_mix_server.ex
+++ Elixir/first_umbrella/apps/first_mix_server/lib/first_mix_server.ex
@@ -12,7 +12,38 @@ defmodule FirstMixServer do
       :world
 
   """
+
+  require Logger
+
   def hello do
     :world
+  end
+
+  def accept(port) do
+    {:ok, socket} =
+      :gen_tcp.listen(port, [:binary, packet: :line, active: false, reuseaddr: true])
+
+    Logger.info("Accepting connections on port #{port}.")
+    loop_acceptor(socket)
+  end
+
+  defp loop_acceptor(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+    serve(client)
+    loop_acceptor(socket)
+  end
+
+  defp serve(socket) do
+    socket |> read_line() |> write_line(socket)
+    serve(socket)
+  end
+
+  defp read_line(socket) do
+    {:ok, line} = :gen_tcp.recv(socket, 0)
+    line
+  end
+
+  defp write_line(line, socket) do
+    :gen_tcp.send(socket, line)
   end
 end
