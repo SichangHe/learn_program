4b2fb28c1cf31787023f988db806dca18bb0e6dc
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2022-12-31 21:30:37 -0600

    Board.position_island/3;Board.all_islands_positioned?/1

--- Elixir/islands_engine/lib/islands_engine/board.ex
+++ Elixir/islands_engine/lib/islands_engine/board.ex
@@ -1,4 +1,25 @@
 defmodule IslandsEngine.Board do
-  @spec new() :: %{}
+  alias IslandsEngine.Island
+
+  @type t :: %{atom => Island.t()}
+
+  @spec new() :: t
   def new(), do: %{}
+
+  @spec position_island(t, atom, Island.t()) :: t
+  def position_island(board, key, island) do
+    if disjoint_existing_islands(board, key, island) do
+      Map.put(board, key, island)
+    else
+      {:error, :overlapping_island}
+    end
+  end
+
+  @spec disjoint_existing_islands(t, atom, Island.t()) :: boolean
+  defp disjoint_existing_islands(board, key, island),
+    do: Enum.all?(board, fn {k, i} -> Island.disjoint?(island, i) or key == k end)
+
+  @spec all_islands_positioned?(t) :: boolean
+  def all_islands_positioned?(board),
+    do: Enum.all?(Island.types(), &Map.has_key?(board, &1))
 end
