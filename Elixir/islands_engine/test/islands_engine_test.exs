4d15f5fee83958285d4996168186b72d79304cb2
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2022-12-31 19:48:16 -0600

    test Island.disjoint?

--- Elixir/islands_engine/test/islands_engine_test.exs
+++ Elixir/islands_engine/test/islands_engine_test.exs
@@ -47,4 +47,16 @@ defmodule IslandsEngineTest.Island do
     assert {:error, :invalid_island_type} = Island.new(:unknown, upper_left)
     assert {:error, :invalid_coordinate} = Island.new(:l_shape, lower_left)
   end
+
+  test "Detect islands disjoint" do
+    {:ok, upper_left} = Coordinate.new(1, 1)
+    {:ok, upper_second_left} = Coordinate.new(1, 2)
+    {:ok, center} = Coordinate.new(5, 5)
+    {:ok, square} = Island.new(:square, upper_left)
+    {:ok, dot} = Island.new(:dot, upper_second_left)
+    {:ok, ell} = Island.new(:l_shape, center)
+    assert not Island.disjoint?(square, dot)
+    assert Island.disjoint?(square, ell)
+    assert Island.disjoint?(dot, ell)
+  end
 end
