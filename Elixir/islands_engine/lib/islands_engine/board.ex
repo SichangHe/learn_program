5c3944f2b1f2e75d597b9b02c201832fd77fb59b
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-01-05 09:55:03 +0800

    Board functions

--- Elixir/islands_engine/lib/islands_engine/board.ex
+++ Elixir/islands_engine/lib/islands_engine/board.ex
@@ -1,5 +1,5 @@
 defmodule IslandsEngine.Board do
-  alias IslandsEngine.Island
+  alias IslandsEngine.{Coordinate, Island}
 
   @type t :: %{atom => Island.t()}
 
@@ -22,4 +22,42 @@ defmodule IslandsEngine.Board do
   @spec all_islands_positioned?(t) :: boolean
   def all_islands_positioned?(board),
     do: Enum.all?(Island.types(), &Map.has_key?(board, &1))
+
+  @spec guess(t, Coordinate.t()) :: {:hit | :miss, atom, :win | :no_win, t}
+  def guess(board, %Coordinate{} = coordinate),
+    do: board |> check_all_islands(coordinate) |> guess_response(board)
+
+  @spec check_all_islands(t, Coordinate.t()) :: {atom, Island.t()} | :miss
+  defp check_all_islands(board, coord) do
+    Enum.find_value(board, :miss, fn {key, island} ->
+      case Island.guess(island, coord) do
+        {:hit, island} -> {key, island}
+        :miss -> false
+      end
+    end)
+  end
+
+  @spec guess_response({atom, Island.t()} | :miss, t) ::
+          {:hit | :miss, atom, :win | :no_win, t}
+  defp guess_response({key, island}, board),
+    do: {:hit, forest_check(board, key), win_check(board), %{board | key => island}}
+
+  defp guess_response(:miss, board), do: {:miss, :none, :no_win, board}
+
+  @spec forest_check(t, atom) :: atom
+  defp forest_check(board, key) do
+    if forested?(board, key), do: key, else: :none
+  end
+
+  @spec forested?(t, atom) :: boolean
+  defp forested?(board, key), do: board |> Map.fetch!(key) |> Island.forested?()
+
+  @spec win_check(t) :: :win | :no_win
+  defp win_check(board) do
+    if all_forested?(board), do: :win, else: :no_win
+  end
+
+  @spec all_forested?(t) :: boolean
+  defp all_forested?(board),
+    do: Enum.all?(board, fn {_, island} -> Island.forested?(island) end)
 end
