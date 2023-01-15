8b6c015a553d31b34f91932c6c8c563f9d8b5c1b
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-01-15 18:38:39 +0800

    game add player;reorganize&separate server code

--- Elixir/islands_engine/lib/islands_engine/game.ex
+++ Elixir/islands_engine/lib/islands_engine/game.ex
@@ -19,19 +19,45 @@ defmodule IslandsEngine.Game.State do
 
   alias __MODULE__
 
-  @type t :: %State{player1: String.t(), player2: String.t(), rules: Rules.t()}
+  @type t :: %State{player1: Player.t(), player2: Player.t(), rules: Rules.t()}
+end
+
+defmodule IslandsEngine.Game.Server do
+  use GenServer
+  alias IslandsEngine.Rules
+  alias IslandsEngine.Game.{Player, State}
+
+  @type call :: {:add_player, String.t()}
+  @type reply_content :: :ok | :error
+
+  @spec init(String.t()) :: {:ok, State.t()}
+  def init(name), do: {:ok, %State{player1: %Player{name: name}}}
+
+  @spec handle_call(call, any, State.t()) :: {:reply, reply_content, State.t()}
+  def handle_call({:add_player, name}, _from, state) do
+    with {:ok, rules} <- Rules.check(state.rules, :add_player) do
+      state |> update_player2_name(name) |> update_rules(rules) |> reply_with(:ok)
+    else
+      :error -> reply_with(state, :error)
+    end
+  end
+
+  @spec update_player2_name(State.t(), String.t()) :: State.t()
+  defp update_player2_name(state, name), do: put_in(state.player2.name, name)
+
+  @spec update_rules(State.t(), Rules.t()) :: State.t()
+  defp update_rules(state, rules), do: %State{state | rules: rules}
+
+  @spec reply_with(State.t(), reply_content) :: {:reply, reply_content, State.t()}
+  defp reply_with(state, reply), do: {:reply, reply, state}
 end
 
 defmodule IslandsEngine.Game do
-  use GenServer
-  alias IslandsEngine.Game.{Player, State}
+  alias __MODULE__.Server
 
-  # Server
-  def init(name) do
-    {:ok, %State{player1: %Player{name: name}}}
-  end
-
-  # Client
   @spec start_link(String.t()) :: GenServer.on_start()
-  def start_link(name) when is_binary(name), do: GenServer.start_link(__MODULE__, name)
+  def start_link(name) when is_binary(name), do: GenServer.start_link(Server, name)
+
+  @spec add_player(pid, String.t()) :: :ok
+  def add_player(game, name) when is_binary(name), do: GenServer.call(game, {:add_player, name})
 end
--- Elixir/islands_engine/test/islands_engine_test.exs
+++ Elixir/islands_engine/test/islands_engine_test.exs
@@ -98,3 +98,13 @@ defmodule IslandsEngineTest.Rules do
     assert :game_over = rules.state
   end
 end
+
+defmodule IslandsEngineTest.Game do
+  use ExUnit.Case
+  alias IslandsEngine.Game
+
+  assert {:ok, game} = Game.start_link("Frank")
+  p2 = "Dweezil"
+  assert :ok = Game.add_player(game, p2)
+  assert ^p2 = :sys.get_state(game).player2.name
+end
