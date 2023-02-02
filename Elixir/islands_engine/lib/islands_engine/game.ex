65208d4e11803184fca935de7adbd14651703992
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-02-02 12:59:05 +0800

    game set_islands

--- Elixir/islands_engine/lib/islands_engine/game.ex
+++ Elixir/islands_engine/lib/islands_engine/game.ex
@@ -20,6 +20,7 @@ defmodule IslandsEngine.Game.State do
   alias __MODULE__
 
   @type t :: %State{player1: Player.t(), player2: Player.t(), rules: Rules.t()}
+  @type players :: :player1 | :player2
 end
 
 defmodule IslandsEngine.Game.Server do
@@ -29,9 +30,13 @@ defmodule IslandsEngine.Game.Server do
 
   @type call ::
           {:add_player, String.t()}
-          | {:position_island, :player1 | :player2, atom, pos_integer, pos_integer}
-  @type reply_content ::
-          :ok | :error | {:error, :invalid_coordinate} | {:error, :invalid_island_type}
+          | {:position_island, State.players(), atom, pos_integer, pos_integer}
+          | {:set_island, State.players()}
+  @type add_player_reply :: :ok | :error
+  @type position_island_reply ::
+          :ok | :error | {:error, :invalid_coordinate | :invalid_island_type}
+  @type set_islands_reply :: {:ok, Board.t()} | :error | {:error, :not_all_islands_positioned}
+  @type reply_content :: add_player_reply | position_island_reply | set_islands_reply
 
   @spec init(String.t()) :: {:ok, State.t()}
   def init(name), do: {:ok, %State{player1: %Player{name: name}}}
@@ -58,6 +63,18 @@ defmodule IslandsEngine.Game.Server do
     end
   end
 
+  def handle_call({:set_islands, player}, _from, state) do
+    board = player_board(state, player)
+
+    with {:ok, rules} <- Rules.check(state.rules, {:set_islands, player}),
+         true <- Board.all_islands_positioned?(board) do
+      state |> update_rules(rules) |> reply_with({:ok, board})
+    else
+      :error -> reply_with(state, :error)
+      false -> reply_with(state, {:error, :not_all_islands_positioned})
+    end
+  end
+
   @spec update_player2_name(State.t(), String.t()) :: State.t()
   defp update_player2_name(state, name), do: put_in(state.player2.name, name)
 
@@ -75,17 +92,22 @@ defmodule IslandsEngine.Game.Server do
 end
 
 defmodule IslandsEngine.Game do
-  alias __MODULE__.Server
+  alias __MODULE__.{Server, State}
+
+  @players [:player1, :player2]
 
   @spec start_link(String.t()) :: GenServer.on_start()
   def start_link(name) when is_binary(name), do: GenServer.start_link(Server, name)
 
-  @spec add_player(pid, String.t()) :: :ok
+  @spec add_player(pid, String.t()) :: Server.add_player_reply()
   def add_player(game, name) when is_binary(name), do: GenServer.call(game, {:add_player, name})
 
-  @spec position_island(pid, :player1 | :player2, atom, pos_integer, pos_integer) ::
-          Server.reply_content()
-  def position_island(game, player, key, row, col) when player in [:player1, :player2] do
-    GenServer.call(game, {:position_island, player, key, row, col})
-  end
+  @spec position_island(pid, State.players(), atom, pos_integer, pos_integer) ::
+          Server.position_island_reply()
+  def position_island(game, player, key, row, col) when player in @players,
+    do: GenServer.call(game, {:position_island, player, key, row, col})
+
+  @spec set_islands(pid, State.players()) :: Server.set_islands_reply()
+  def set_islands(game, player) when player in @players,
+    do: GenServer.call(game, {:set_islands, player})
 end
--- Elixir/islands_engine/test/islands_engine/game_test.exs
+++ Elixir/islands_engine/test/islands_engine/game_test.exs
@@ -1,6 +1,6 @@
 defmodule IslandsEngineTest.Game do
   use ExUnit.Case
-  alias IslandsEngine.{Game, Rules}
+  alias IslandsEngine.Game
 
   test "Walk through" do
     assert {:ok, game} = Game.start_link("Frank")
@@ -9,11 +9,22 @@ defmodule IslandsEngineTest.Game do
     state = :sys.get_state(game)
     assert ^p2 = state.player2.name
     assert :players_set = state.rules.state
+
     assert :ok = Game.position_island(game, :player1, :square, 1, 1)
     assert {:error, :invalid_coordinate} = Game.position_island(game, :player1, :dot, 12, 1)
     assert {:error, :invalid_island_type} = Game.position_island(game, :player1, :wrong, 1, 1)
     assert {:error, :invalid_coordinate} = Game.position_island(game, :player1, :l_shape, 10, 10)
-    :sys.replace_state(game, &%{&1 | rules: %Rules{state: :player1_turn}})
-    assert :error = Game.position_island(game, :player1, :dot, 5, 5)
+
+    assert {:error, :not_all_islands_positioned} = Game.set_islands(game, :player1)
+
+    assert :ok = Game.position_island(game, :player1, :atoll, 3, 1)
+    assert :ok = Game.position_island(game, :player1, :dot, 4, 1)
+    assert :ok = Game.position_island(game, :player1, :l_shape, 6, 1)
+    assert :ok = Game.position_island(game, :player1, :s_shape, 2, 5)
+
+    assert {:ok, _board} = Game.set_islands(game, :player1)
+    state = :sys.get_state(game)
+    assert :islands_set = state.rules.player1
+    assert :players_set = state.rules.state
   end
 end
