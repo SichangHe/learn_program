94fb400de6a8a834606a8215663604c1b703ab71
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-02-26 17:59:58 +0800

    done android quick start

--- Android/quick_start/app/src/main/java/com/youknowwho/quickstart/MainActivity.kt
+++ Android/quick_start/app/src/main/java/com/youknowwho/quickstart/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.youknowwho.quickstart.ui.theme.QuickStartTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,7 +21,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuickStartTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
                     Greeting("Android")
                 }
             }
@@ -28,7 +34,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Surface(color = Color.Cyan) {
+        Text(text = "Hey you, I am $name!", modifier = Modifier.padding(24.dp))
+    }
 }
 
 @Preview(showBackground = true)
