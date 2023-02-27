07cac6fceac8dd7f6eb1922358d0019a2fe9e054
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-02-27 20:58:23 +0800

    Jetpack Compose Tutorial

--- Android/quick_start/app/src/main/java/com/youknowwho/quickstart/MainActivity.kt
+++ Android/quick_start/app/src/main/java/com/youknowwho/quickstart/MainActivity.kt
@@ -3,12 +3,16 @@ package com.youknowwho.quickstart
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,10 +26,10 @@ class MainActivity : ComponentActivity() {
             QuickStartTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
                 ) {
                     Greeting("Android")
+                    Conversation(SampleData.conversationSample)
                 }
             }
         }
@@ -41,8 +45,81 @@ fun Greeting(name: String) {
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+private fun PreviewGreeting() {
     QuickStartTheme {
         Greeting("Android")
+    }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Spacer(modifier = Modifier.width(8.dp))
+
+        var isExpanded by remember {
+            mutableStateOf(false)
+        }
+        val surfaceColor by animateColorAsState(
+            targetValue = if (isExpanded) {
+                MaterialTheme.colors.primary
+            } else {
+                MaterialTheme.colors.surface
+            }
+        )
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if (isExpanded) {
+                        Int.MAX_VALUE
+                    } else {
+                        1
+                    },
+                    style = MaterialTheme.typography.body2
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    QuickStartTheme {
+        Surface {
+            MessageCard(msg = Message("Colleague", "Hey, take a look at Jetpack Compose!"))
+        }
+    }
+}
+
+@Composable
+fun Conversation(msgs: List<Message>) {
+    LazyColumn {
+        items(msgs) { msg -> MessageCard(msg) }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    QuickStartTheme {
+        Conversation(SampleData.conversationSample)
     }
 }
