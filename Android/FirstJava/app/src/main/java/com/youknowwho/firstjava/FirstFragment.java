f583154ce3d41790613b462378d88438c441ee03
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-03-10 12:27:39 +0800

    click toast

--- Android/FirstJava/app/src/main/java/com/youknowwho/firstjava/FirstFragment.java
+++ Android/FirstJava/app/src/main/java/com/youknowwho/firstjava/FirstFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -29,13 +30,15 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.btnToast.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-            }
-        });
+        binding.btnRand.setOnClickListener(
+                view1 -> NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment)
+        );
+        binding.btnToast.setOnClickListener(
+                view1 -> {
+                    Toast toast = Toast.makeText(getActivity(), R.string.toast_txt, Toast.LENGTH_LONG);
+                    toast.show();
+                });
     }
 
     @Override
