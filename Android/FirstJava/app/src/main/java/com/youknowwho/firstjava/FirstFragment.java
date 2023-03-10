9692e63dd566e02af009f7a09c56187e85e27beb
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-03-11 07:44:19 +0800

    RxJava for nonblocking request

--- Android/FirstJava/app/src/main/java/com/youknowwho/firstjava/FirstFragment.java
+++ Android/FirstJava/app/src/main/java/com/youknowwho/firstjava/FirstFragment.java
@@ -1,7 +1,6 @@
 package com.youknowwho.firstjava;
 
 import android.os.Bundle;
-import android.os.StrictMode;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +16,9 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -36,15 +38,8 @@ public class FirstFragment extends Fragment {
             @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-        // TODO: make network requests async and remove this.
-        // Cancel strict mode to enable blocking network.
-        StrictMode.ThreadPolicy gfgPolicy =
-                new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(gfgPolicy);
-
         binding = FragmentFirstBinding.inflate(inflater, container, false);
         return binding.getRoot();
-
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
@@ -53,8 +48,7 @@ public class FirstFragment extends Fragment {
         binding.btnRand.setOnClickListener(
                 view1 -> NavHostFragment.findNavController(FirstFragment.this)
                         .navigate(R.id.action_FirstFragment_to_SecondFragment));
-        binding.btnToast.setOnClickListener(
-                view1 -> {
+        binding.btnToast.setOnClickListener(view1 -> Flowable.fromCallable(() -> {
                     GitHubService service = retrofit.create(GitHubService.class);
                     Call<List<Contributor>> call = service.listRepo("square", "retrofit");
                     String txt;
@@ -69,10 +63,14 @@ public class FirstFragment extends Fragment {
                     } catch (IOException e) {
                         txt = e.toString();
                     }
-                    Toast.makeText(
-                                    getActivity(), txt, Toast.LENGTH_LONG)
-                            .show();
-                });
+                    return txt;
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(txt -> Toast.makeText(
+                                        getActivity(), txt, Toast.LENGTH_LONG)
+                                .show(),
+                        System.out::println));
         binding.btnCount.setOnClickListener(
                 view1 -> binding.textviewFirst.setText(
                         String.format(Locale.getDefault(), "%d", ++count)));
