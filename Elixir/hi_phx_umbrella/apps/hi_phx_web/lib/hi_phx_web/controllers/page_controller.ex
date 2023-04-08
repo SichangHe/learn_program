42490d8af11e2b57b64bf55958dfc982d6b49101
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-04-08 20:29:27 +0800

    controllers are plug;plug can have condition

--- Elixir/hi_phx_umbrella/apps/hi_phx_web/lib/hi_phx_web/controllers/page_controller.ex
+++ Elixir/hi_phx_umbrella/apps/hi_phx_web/lib/hi_phx_web/controllers/page_controller.ex
@@ -1,5 +1,7 @@
 defmodule HiPhxWeb.PageController do
   use HiPhxWeb, :controller
+  # Append locale to connection.
+  plug HiPhxWeb.Plugs.Locale, "en" when action in [:index]
 
   def index(conn, _params) do
     render(conn, "index.html")
