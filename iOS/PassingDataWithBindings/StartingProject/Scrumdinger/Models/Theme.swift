0f6a03c07459ba3e6989d4023fa0b7842e61ef94
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-08-05 17:52:30 +0800

    working theme picker

--- iOS/PassingDataWithBindings/StartingProject/Scrumdinger/Models/Theme.swift
+++ iOS/PassingDataWithBindings/StartingProject/Scrumdinger/Models/Theme.swift
@@ -4,7 +4,7 @@
 
 import SwiftUI
 
-enum Theme: String {
+enum Theme: String, CaseIterable, Identifiable {
     case bubblegum
     case buttercup
     case indigo
@@ -21,17 +21,23 @@ enum Theme: String {
     case tan
     case teal
     case yellow
-    
+
     var accentColor: Color {
         switch self {
         case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow: return .black
         case .indigo, .magenta, .navy, .oxblood, .purple: return .white
         }
     }
+
     var mainColor: Color {
         Color(rawValue)
     }
+
     var name: String {
         rawValue.capitalized
+    }
+
+    var id: String {
+        name
     }
 }
